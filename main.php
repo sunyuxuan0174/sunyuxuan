d6982286d59701d69c10d69579c99f435ebb50a9
sunyuxuan0174 <46037074+sunyuxuan0174@users.noreply.github.com>
2019-01-02 08:41:20 +0800

    Add files via upload

---
+++ main.php
@@ -0,0 +1,13 @@
+<html>
+	<head>
+		<title>新闻管理</title>
+	</head>
+	<meta charset='utf-8'>
+	<frameset rows="20%,*">
+		<frame src='top.php'>
+		<frameset cols='20%,*'>
+			<frame src='left.php'>
+			<frame src='right.php' name='right'>
+		</frameset>
+	</frameset>
+</html>
